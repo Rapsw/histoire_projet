a0ce8c4c9ae7a7937d347d4cc16df1780d3e0d1f
Rapsw Calapsu <rapswarts@gmail.com>
2022-07-05 13:46:57 +0200

    ajout fonction mettre à jour comment

--- croud.py
+++ croud.py
@@ -1,32 +1,42 @@
 import sqlite3
-
-from sympy import Id
+from datetime import datetime
 
 # création des fonctions de la table Chapter
 
-def creation_chapitre():
+def creation_chapitre(Summary):
     connexion = sqlite3.connect("bdd.db")
     curseur = connexion.cursor()
-    Summary = str(input("Entrer un nom de chapitre"))
     curseur.execute("INSERT INTO Chapter VALUES(?,?)",(None, Summary))
     connexion.commit()
     connexion.close()
 
 # fonction pour suprimer chapitres
 
-def supprimer_chapitre():
+def supprimer_chapitre(ChapterID):
     connexion = sqlite3.connect("bdd.db")
     curseur = connexion.cursor()
-    curseur.execute("DELETE FROM Chapter WHERE ChapterID = ?;", (Id,))
+    curseur.execute("DELETE FROM Chapter WHERE ChapterID = ?;", (ChapterID,))
     connexion.commit()
     connexion.close()
 
 # fonction pour mettre à jour chapitre
 
-def maj_chapitre():
+def maj_chapitre(ChapterID, new_summary):
     connexion = sqlite3.connect("bdd.db")
     curseur = connexion.cursor()
-    curseur.execute("UPDATE Chapter SET Summary = ? WHERE ChapterID = ?", (id))
+    curseur.execute("UPDATE Chapter SET Summary = ? WHERE ChapterID = ?", (new_summary, ChapterID))
     connexion.commit()
     connexion.close()
+
+# fonction pour metre à jour comment
+
+def maj_comment(CommentID, new_comment, new_text):
+    connexion = sqlite3.connect("bdd.db")
+    curseur = connexion.cursor()
+        if new_comment is not None
+            curseur.execute("UPDATE Comment SET Date = ? WHERE CommentID = ?", (new_comment, CommentID))
+        if new_text is not None
+            curseur.execute("UPDATE Comment SET Text = ? WHERE CommentID = ?", (new_text, CommentID))
+        connexion.commit()
+        connexion.close()
 
