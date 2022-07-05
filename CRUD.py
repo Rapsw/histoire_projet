34332219bb189608ece0a361fb67eef318b2db49
NinaNusb <nina.nusbaumer@gmail.com>
2022-07-05 10:30:15 +0200

    complétion Caracter CRUD

--- CRUD.py
+++ CRUD.py
@@ -4,9 +4,9 @@ import sqlite3
 def createCaracter():
     connexion = sqlite3.connect("bdd.db")
     curseur = connexion.cursor()
-    FirstName = str(input("Entrez le prénom du personnage: "))
-    LastName = str(input("Entrez le nom du personnage: "))
-    Resume = str(input("Décrivez succinctement l'histoire du personnage: "))
+    FirstName = input("Entrez le prénom du personnage: ")
+    LastName = input("Entrez le nom du personnage: ")
+    Resume = input("Décrivez succinctement l'histoire du personnage: ")
     curseur.execute("INSERT INTO Caracter VALUES(?,?,?,?)", (None,FirstName,LastName, Resume))
     connexion.commit()
     connexion.close()
@@ -16,4 +16,27 @@ def readCaracter():
     curseur = connexion.cursor()
     curseur.execute("SELECT FirstName, LastName, Resume FROM Caracter GROUP BY CaracterID")
     curseur.fetchall()
+
+def updateCaracter(FirstName = None, LastName = None, Resume = None):
+    connexion = sqlite3.connect("bdd.db")
+    curseur = connexion.cursor()
+    NewFirstName= input("Entrez le nouveau prénom du personnage: ")
+    NewLastName = input("Entrez le nouveau nom du personnage: ")
+    curseur.execute("""UPDATE Caracter 
+    if FirstName is not None:
+        SET FirstName = NewFirstName""")
+    curseur.execute("""UPDATE Caracter 
+    if LastName is not None:
+        SET LastName = NewLastName""")
+    connexion.commit()
+    connexion.close()
+
+def deleteCaracter():
+    connexion = sqlite3.connect("bdd.db")
+    curseur = connexion.cursor()
+    CaracterToBeDeleted = int(input("Entrez l'ID du personnage à supprimer: "))
+    curseur.execute("DELETE FROM Caracter WHERE CaracterID = CaracterToBeDeleted")
+    connexion.commit()
+    connexion.close()
+
 
